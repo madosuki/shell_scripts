bcc6403c81b7d986f1de3e282716769f0c2ffa62
madosuki <yurikasamaaikatsu@gmail.com>
2016-11-26 20:07:42 +0900

    change3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Shell Scripts
-"uninstall_vs.sh" is Uninstall Script for "Visual Studio for Mac Preview"
+"uninstall_vs.sh"は"Visual Studio for Mac Preview"関連のファイルを削除するスクリプトです。結構適当にやっているので参考程度にしてください。
--- uninstall_vs.sh
+++ uninstall_vs.sh
@@ -4,6 +4,12 @@ rm -rf $HOME/Library/Developer/Xamarin
 rm -rf $HOME/Library/Preferences/VisualStudio
 rm -rf $HOME/Library/Caches/VisualStudio
 rm -rf $HOME/Library/Logs/VisualStudio
+rm -rf $HOME/Library/Caches/XamarinInstaller
+rm -rf $HOME/Library/Xamarin
+rm -rf $HOME/.local/share/Xamarin*
+rm -rf $HOME/.local/share/MonoForAndroid
+rm -rf $HOME/Library/Caches/com.xamarin.fontconfig
+rm -rf $HOME/Library/Caches/com.xamarin.universalinstaller
 sudo rm -rf /Developer/MonoAndroid
 sudo rm -rf /Developer/MonoTouch
 sudo rm -rf /Applications/Visual\ Studio.app
