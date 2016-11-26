8d3d8da00595720f613b48221d1484b876471486
madosuki <yurikasamaaikatsu@gmail.com>
2016-11-26 15:13:55 +0900

    change

--- uninstall_vs.sh
+++ uninstall_vs.sh
@@ -4,8 +4,10 @@ rm -rf $HOME/Library/Developer/Xamarin
 rm -rf $HOME/Library/Preferences/VisualStudio
 rm -rf $HOME/Library/Caches/VisualStudio
 rm -rf $HOME/Library/Logs/VisualStudio
-rm -rf /Developer/MonoAndroid
-rm -rf /Developer/MonoTouch
-rm -rf /Applications/Visual\ Studio.app
-rm -rf /Applications/Xamarin\ Workbooks.app
-rm -rf /Applications/Xamarin\ Profiler.app
+sudo rm -rf /Developer/MonoAndroid
+sudo rm -rf /Developer/MonoTouch
+sudo rm -rf /Applications/Visual\ Studio.app
+sudo rm -rf /Applications/Xamarin\ Workbooks.app
+sudo rm -rf /Applications/Xamarin\ Profiler.app
+sudo /Library/Frameworks/Mono.framework
+sudo /Library/Frameworks/Xamarin*
