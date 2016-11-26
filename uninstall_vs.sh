781432bac66bf199f95b4c1986716a1c7030b7f6
madosuki <yurikasamaaikatsu@gmail.com>
2016-11-27 00:28:35 +0900

    add line

--- uninstall_vs.sh
+++ uninstall_vs.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-rm -rf $HOME/Library/Developer/Xamarin
-rm -rf $HOME/Library/Preferences/VisualStudio
-rm -rf $HOME/Library/Caches/VisualStudio
-rm -rf $HOME/Library/Logs/VisualStudio
-rm -rf $HOME/Library/Caches/Xamarin
-rm -rf $HOME/Library/Caches/XamarinInstaller
-rm -rf $HOME/Library/Xamarin
-rm -rf $HOME/.local/share/Xamarin*
-rm -rf $HOME/.local/share/MonoForAndroid
 sudo rm -rf $HOME/Library/Caches/com.xamarin.fontconfig
 sudo rm -rf $HOME/Library/Caches/com.xamarin.universalinstaller
 sudo rm -rf /Developer/MonoAndroid
@@ -18,3 +9,19 @@ sudo rm -rf /Applications/Xamarin\ Workbooks.app
 sudo rm -rf /Applications/Xamarin\ Profiler.app
 sudo rm -rf /Library/Frameworks/Mono.framework
 sudo rm -rf /Library/Frameworks/Xamarin*
+sudo pkgutil --forget com.xamarin.Inspector
+sudo pkgutil --forget com.xamarin.profiler
+sudo pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
+sudo pkgutil --forget com.xamarin.xamarin.ios.pkg
+sudo pkgutil --forget com.xamarin.monotouch.pkg
+sudo pkgutil --forget com.xamarin.android.pkg
+rm -rf $HOME/Library/Developer/Xamarin
+rm -rf $HOME/Library/Preferences/VisualStudio
+rm -rf $HOME/Library/Caches/VisualStudio
+rm -rf $HOME/Library/Logs/VisualStudio
+rm -rf $HOME/Library/Caches/Xamarin
+rm -rf $HOME/Library/Caches/XamarinInstaller
+rm -rf $HOME/Library/Xamarin
+rm -rf $HOME/.local/share/Xamarin*
+rm -rf $HOME/.local/share/MonoForAndroid
+rm -rf $HOME/Library/Logs/XamarinInstaller
