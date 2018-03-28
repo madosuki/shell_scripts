121d3ea73a1a24025546bbda348fb2db4a4df8a1
madosuki <yurikasamaaikatsu@gmail.com>
2018-03-28 10:20:08 +0900

    fix h264_enc.sh

--- h264_enc.sh
+++ h264_enc.sh
@@ -8,7 +8,6 @@ x264_params="-x264-params "crf=18:keyint=300:me=umh:b-adapt=2:partitions=all:${c
 # libfdk_aac or aac(ffmpeg native aac encoder)
 audio_kb='256k'
 audiooption="libfdk_aac -b:a ${audio_kb}"
-setfps="-r 30000/10001"
 yuv="-pix_fmt yuv420p"
 out="EP${2}.mp4"
 # bwdif option order mode(0=from 2field, 1=from one side field) to parity(either priority top or bottom) to deint(0=all,1=target interlaced field)
@@ -16,4 +15,4 @@ out="EP${2}.mp4"
 # 24fps bwdif=0:-1:1,decimate
 # 30fps bwdif=0:-1:1
 vf='bwdif=0:-1:1,crop=704:480:8:0'
-ffmpeg -i ${1} -c:v libx264 ${x264_params} -c:a ${audiooption}  ${yuv} -lavfi ${vf} ${setfps} ${out}
+ffmpeg -i ${1} -c:v libx264 ${x264_params} -c:a ${audiooption} ${yuv} -lavfi ${vf} -r 30000/1001 ${out}
