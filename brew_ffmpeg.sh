808e47912b01dc83074ef5341a7abd97f72742f8
madosuki <yurikasamaaikatsu@gmail.com>
2018-04-17 15:20:29 +0900

    change brew_ffmpeg

--- brew_ffmpeg.sh
+++ brew_ffmpeg.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-brew install ffmpeg --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libvorbis --with-libvpx --with-opus --with-x265 --with-rtmpdump --withh-rubberband --with-tools --with-xz --with-zimg --with-webp --with-opencore-amr --wtih-openssl --with-openjpeg --wtih-snappy --HEAD
+check=`brew list | grep ffmpeg`
+
+function install_ffmpeg() {
+    brew install ffmpeg --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libvorbis --with-libvpx --with-opus --with-x265 --with-rtmpdump --withh-rubberband --with-tools --with-xz --with-zimg --with-webp --with-opencore-amr --wtih-openssl --with-openjpeg --wtih-snappy --HEAD
+    brew install mpv --with-bundle
+}
+
+function uninstall_ffmpeg() {
+    brew unistall mpv
+    brew uninstall ffmpeg --ignore-dependencies
+}
+
+if [ -z ${check} ]; then
+    install_ffmpeg
+else
+    install_ffmpeg()
+fi
+
