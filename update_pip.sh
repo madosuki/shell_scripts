6632bf6abfd43d5e8d37582919c3172020583f32
madosuki <yurikasamaaikatsu@gmail.com>
2024-07-05 16:02:23 +0900

    update

--- update_pip.sh
+++ update_pip.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-function AutoText() {
-    echo "Please 2 or 3 key."
-}
-AutoText
-read version
-
-if [ ${version} = "3" ]; then
-    pip3 list --format=columns | awk '{if(NR>2)print $1}' | xargs pip3 install -U
-elif [ ${version} = "2" ]; then
-    pip2 list --format=columns | awk '{if(NR>2)print $1}' | xargs pip2 install -U
-else
-    AutoText
-fi
+python -m pip list | awk '{if (NR > 2) {if ($1 == "") { exit } else { print $1 }}}' | xargs python -m pip install -U
